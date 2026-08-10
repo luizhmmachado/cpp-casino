711546b404c8ee3b4daaf852d738a91f4b6b41e2
luiz.machado <luiz.machado@ciss.com.br>
2026-08-10 17:15:09 -0300

    fix: #CC-36 Adiciona fonts

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QFontDatabase>
+#include <QDebug>
 
-#include <control/blakcjack/blackjackcontrol.h>
+#include <control/blackjack/blackjackcontrol.h>
 #include <control/database/databasecontrol.h>
 #include <control/horserace/horseracecontrol.h>
 
@@ -11,11 +13,18 @@ int main( int argc, char* argv[] ) {
 #endif
     QGuiApplication app( argc, argv );
 
+    const int fontId = QFontDatabase::addApplicationFont( ":/ui/theme/fonts/PressStart2P-Regular.ttf" );
+    if ( fontId < 0 ) {
+        qWarning() << "Failed to load bundled font: PressStart2P-Regular.ttf";
+    }
+
     qmlRegisterType<BlackJackControl>( "BlackJackControl", 1, 0, "BlackJackControl" );
     qmlRegisterType<DataBaseControl>( "DataBaseControl", 1, 0, "DataBaseControl" );
     qmlRegisterType<HorseRaceControl>( "HorseRaceControl", 1, 0, "HorseRaceControl" );
-    qmlRegisterSingletonType( QUrl( QStringLiteral( "qrc:/theme/Colors.qml" ) ),
+    qmlRegisterSingletonType( QUrl( QStringLiteral( "qrc:/ui/theme/Colors.qml" ) ),
                               "Colors", 1, 0, "Colors" );
+    qmlRegisterSingletonType( QUrl( QStringLiteral( "qrc:/ui/theme/Fonts.qml" ) ),
+                              "Fonts", 1, 0, "Fonts" );
 
     QQmlApplicationEngine engine;
     engine.addImportPath( "qrc:/" );
